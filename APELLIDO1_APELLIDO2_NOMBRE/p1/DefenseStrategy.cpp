a2038ef8d1e2799e295ed61e1ec41f709ae7aa8e
Kali <luis.torresmo@alum.uca.es>
2021-10-20 19:52:31 +0100

    nuevos cambios
    No he terminado la parte del mapa del ej 1

--- APELLIDO1_APELLIDO2_NOMBRE/p1/DefenseStrategy.cpp
+++ APELLIDO1_APELLIDO2_NOMBRE/p1/DefenseStrategy.cpp
@@ -21,7 +21,7 @@ using namespace Asedio;
 Vector3 cellCenterToPosition(int i, int j, float cellWidth, float cellHeight){ return Vector3((j * cellWidth) + cellWidth * 0.5f, (i * cellHeight) + cellHeight * 0.5f, 0); }
 
 float cellValue(int row, int col, bool** freeCells, int nCellsWidth, int nCellsHeight
-	, float mapWidth, float mapHeight, List<Object*> obstacles, List<Defense*> defenses) {
+	, float mapWidth, float mapHeight, List<Object*> obstacles, List<Defense*> defenses, List<Map*> map) {
     float value = 0;
 
     float cellWidth = mapWidth / nCellsWidth;
@@ -32,20 +32,19 @@ float cellValue(int row, int col, bool** freeCells, int nCellsWidth, int nCellsH
     //  -Cuanto mas lejos este de un obstacúlo mejor
     //Antes que nada debemos determinar la celda, eso lo haremos con la funcion que se nos proporciona en preguntas frecuentes.
     
-    
+    Vector3 cellInPosition = cellCenterToPosition(row, col, cellWidth, cellHeight);    
 
-    //COMPROBACION DE SI ESTA POR EL CENTRO DEL MAPA, HACER OTRO DIA
-
-
-
-
-
+    //El primer factor de valor, que esté lejos de un obstaculo
     List<Object*>::iterator currentObstacle = obstacles.begin();
     while (currentObstacle != obstacles.end()){
         //Como es mejor cuanto mas lejos este de un obstaculo mejor, esta distancia se sumara al value
         value += (*currentObstacle)->position.subtract(cellInPosition).length();
         ++currentObstacle; 
     }
+
+    //El segundo factor de valor
+    //NOSE TIO ESTOY TROLLList <Map*>::iterator currentPosition = map.//
+
     
     
 	return value; // implemente aqui la funci�n que asigna valores a las celdas
